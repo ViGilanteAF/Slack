8a505136c42a0f1130ddd2f9a54c2296e157fe8e
Citizen <40601526+ViGilanteAF@users.noreply.github.com>
2022-09-16 01:32:30 +0900

    Changes to be committed: 	new file:   anest/.env 	new file:   anest/.env.development 	new file:   anest/.env.production 	modified:   anest/package-lock.json 	modified:   anest/package.json 	modified:   anest/src/app.controller.ts 	modified:   anest/src/app.module.ts 	modified:   anest/src/app.service.ts

--- anest/src/app.controller.ts
+++ anest/src/app.controller.ts
@@ -1,19 +1,15 @@
 import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('abc')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   //req, res에 대하여 알고있음 
 
-  @Get('User') //Get /abc/User
-  getUser(): string {
-    return this.appService.getUser();
-  }
+  @Get()
+  getHello(){
+    return this.appService.getHello();
 
-  @Post('user') //POST /abc/User
-  postUser(): string {
-    return this.appService.postUser();
   }
 }
--- anest/src/app.service.ts
+++ anest/src/app.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  getUser(): string {
-    return 'Hello World!';
+  constructor(private readonly configService: ConfigService){}
+
+  async getHello(): Promise<string> {
+    return this.configService.get('DB_PASSWORD');
+    //process.env.DB_PASSWORD
   }
 
-  postUser() : string{
-    return 'post succeedded';
-  }
 }
