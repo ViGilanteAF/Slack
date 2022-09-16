b9dad5e10ff90393692f40fae86db3cf288d4bb6
Citizen <40601526+ViGilanteAF@users.noreply.github.com>
2022-09-17 02:47:32 +0900

    Changes to be committed: 	modified:   anest/src/app.module.ts 	new file:   anest/src/logger.middleware.ts
    LoggerMiddleware로 mogan 처럼 logging

--- anest/src/app.module.ts
+++ anest/src/app.module.ts
@@ -1,7 +1,9 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MiddlewareBuilder } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './logger.middleware';
 
 
 @Module({
@@ -9,4 +11,8 @@ import { AppService } from './app.service';
   controllers: [AppController],
   providers: [AppService, ConfigService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
