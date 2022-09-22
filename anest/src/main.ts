14a76b61a15a9c4893a0fd9c263550f4b9b076c0
Citizen <40601526+ViGilanteAF@users.noreply.github.com>
2022-09-23 02:46:48 +0900

    Changes to be committed: 	modified:   anest/src/main.ts
    Changes not staged for commit:
    	modified:   anest/.env
    	modified:   anest/package-lock.json
    	modified:   anest/package.json
    	modified:   anest/src/app.module.ts
    	modified:   anest/src/app.service.ts
    	modified:   anest/src/channels/channels.module.ts
    	modified:   anest/src/dms/dms.controller.ts
    	modified:   anest/src/users/dto/join.request.dto.ts
    	modified:   anest/src/users/users.controller.ts
    	modified:   anest/src/users/users.service.ts
    	modified:   anest/src/workspaces/workspaces.module.ts
    
    Untracked files:
    	anest/src/channels/channels.controller.spec.ts
    	anest/src/channels/channels.controller.ts
    	anest/src/channels/channels.service.spec.ts
    	anest/src/channels/channels.service.ts
    	anest/src/common/
    	anest/src/workspaces/workspaces.controller.spec.ts
    	anest/src/workspaces/workspaces.controller.ts
    	anest/src/workspaces/workspaces.service.spec.ts
    	anest/src/workspaces/workspaces.service.ts

--- anest/src/main.ts
+++ anest/src/main.ts
@@ -1,16 +1,46 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import passport from 'passport';
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 
 declare const module: any;
-
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
-  await app.listen(port);
-  console.log(`listening on port ${port}`)
 
-  if (module.hot){
+  const config = new DocumentBuilder()
+    .setTitle('Sleact API')
+    .setDescription('Sleact 개발을 위해 만들어진 API 문서입니다.')
+    .setVersion('1.0')
+    .addCookieAuth('connect.sid')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.use(cookieParser());
+
+  app.use(
+    session({
+      resave: false,
+      saveUninitialized: false,
+      secret: process.env.COOKIE_SECRET,
+      cookie: {
+        httpOnly: true,
+      },
+    }),
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  await app.listen(port);
+  console.log(`listening on port ${port}`);
+
+  if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
   }
