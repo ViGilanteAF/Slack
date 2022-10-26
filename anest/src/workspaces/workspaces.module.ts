419cb6f10ec9b1f6a7ee0f8724c79d1823c13150
Citizen <40601526+ViGilanteAF@users.noreply.github.com>
2022-10-26 18:08:52 +0900

    Constructor Injection

--- anest/src/workspaces/workspaces.module.ts
+++ anest/src/workspaces/workspaces.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { WorkspacesService } from './workspaces.service';
 import { WorkspacesController } from './workspaces.controller';
+import { WorkspaceMembers } from 'src/entities/WorkspaceMembers';
+import { ChannelMembers } from 'src/entities/ChannelMembers';
+import { Channels } from 'src/entities/Channels';
+import { Users } from 'src/entities/Users';
+import { Workspaces } from 'src/entities/Workspaces';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      WorkspaceMembers,
+      Workspaces,
+      ChannelMembers,
+      Channels,
+      Users,
+    ]),
+  ],
   providers: [WorkspacesService],
   controllers: [WorkspacesController],
 })
--- anest/src/workspaces/workspaces.service.ts
+++ anest/src/workspaces/workspaces.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ChannelMembers } from 'src/entities/ChannelMembers';
+import { Channels } from 'src/entities/Channels';
+import { Users } from 'src/entities/Users';
+import { WorkspaceMembers } from 'src/entities/WorkspaceMembers';
+import { Workspaces } from 'src/entities/Workspaces';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class WorkspacesService {}
+export class WorkspacesService {
+  constructor(
+    @InjectRepository(Workspaces)
+    private workspacesRepository: Repository<Workspaces>,
+    @InjectRepository(Channels)
+    private channelsRepository: Repository<Channels>,
+    @InjectRepository(WorkspaceMembers)
+    private workspaceMembersRepository: Repository<WorkspaceMembers>,
+    @InjectRepository(ChannelMembers)
+    private channelMembersReporitory: Repository<ChannelMembers>,
+    @InjectRepository(Users)
+    private userRepository: Repository<Users>,
+  ) {}
+}
