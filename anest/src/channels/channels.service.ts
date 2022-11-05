13dc04f14041ad635caa58b7a9d9bd9d1f8736fc
Citizen <40601526+ViGilanteAF@users.noreply.github.com>
2022-11-05 11:47:08 +0900

    Typeorm Query operator

--- anest/src/channels/channels.service.ts
+++ anest/src/channels/channels.service.ts
@@ -1,11 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ChannelChats } from 'src/entities/ChannelChats';
 import { ChannelMembers } from 'src/entities/ChannelMembers';
 import { Channels } from 'src/entities/Channels';
 import { Users } from 'src/entities/Users';
 import { Workspaces } from 'src/entities/Workspaces';
-import { Repository } from 'typeorm';
+import { MoreThan, Repository } from 'typeorm';
+import { runInThisContext } from 'vm';
 
 /** socket io, file uploade */
 @Injectable()
@@ -50,6 +51,109 @@ export class ChannelsService {
     return this.channelsRepository.findOne({
       where: { name },
       relations: ['Workspace'],
+    });
+  }
+
+  async createWorkspaceChannels(url: string, name: string, myId: number) {
+    const workspace = await this.workspacesRepository.findOne({
+      where: { url },
+    });
+    const channel = new Channels();
+    channel.name = name;
+    channel.WorkspaceId = workspace.id;
+    const channelReturned = await this.channelsRepository.save(channel);
+    const channelMember = new ChannelMembers();
+    channelMember.UserId = myId;
+    channelMember.ChannelId = channelReturned.id;
+    await this.channelMembersRepository.save(channelMember);
+  }
+
+  async getWorkspaceChannelMembers(url: string, name: string) {
+    return this.usersRepository
+      .createQueryBuilder('user')
+      .innerJoin('user.Channels', 'channels', 'channels.name = :name', {
+        name,
+      })
+      .innerJoin('channels.Workspace', 'workspace', 'sorkspace.url = :url', {
+        url,
+      })
+      .getMany();
+  }
+
+  async createWorkspaceChannelMembers(url, name, email) {
+    const channel = await this.channelsRepository
+      .createQueryBuilder('channel')
+      .innerJoin('channel.Workspace', 'workspace', 'workspace.url = :url', {
+        url,
+      })
+      .where('channel.name = :name', { name })
+      .getOne();
+    if (!channel) {
+      throw new NotFoundException('채널이 존재하지 않습니다!!!');
+    }
+
+    const user = await this.usersRepository
+      .createQueryBuilder('user')
+      .where('user.email = :email', { email })
+      .innerJoin('user.Workspaces', 'workspace', 'workspace.url = :url', {
+        url,
+      })
+      .getOne();
+    if (!user) {
+      throw new NotFoundException('사용자가 존재 하지 않습니다!');
+    }
+    const channelMember = new ChannelMembers();
+    channelMember.ChannelId = channel.id;
+    channelMember.UserId = user.id;
+    await this.channelMembersRepository.save(channelMember);
+  }
+
+  async getWorkspaceChannelChats(
+    url: string,
+    name: string,
+    perPage: number,
+    page: number,
+  ) {
+    return (
+      /** Slack 내에 챗팅 */
+      this.channelChatsRepository
+        .createQueryBuilder('channelChats')
+        /**index 걸기 */
+        .innerJoin('channelChats.channel', 'channel', 'channel.name = :name', {
+          name,
+        })
+        /**index 걸기 */
+        .innerJoin('channel.Workspace', 'workspace', 'workspace.url = :url', {
+          url,
+        })
+        /**사용자 정보 가지고 오기 */
+        .innerJoinAndSelect('channelChats.User', 'user')
+
+        .orderBy('channelChats.createdAt', 'DESC')
+        /**한번에 보여주는 양 설정( limit) */
+        .take(perPage)
+        .skip(perPage * (page - 1))
+        .getMany()
+    );
+  }
+
+  /**채널 내에서 내가 읽지 않은 메세지 수 보여주기 */
+  async getChannelUnreadsCount(url, name, after) {
+    const channel = await this.channelsRepository
+      .createQueryBuilder('channel')
+      .innerJoin('channel.Workspace', 'workspace', 'workspace.url = :url', {
+        url,
+      })
+      .where('channel.name = :name', { name })
+      .getOne();
+    return this.channelChatsRepository.count({
+      //COUNT(*)
+      where: {
+        ChannelId: channel.id,
+        createdAt: MoreThan(new Date(after)), //createdAt > "2021-11-05"
+
+        //orkhan.gitbook.io/typeorm/docs/find-oprion
+      },
     });
   }
 }
